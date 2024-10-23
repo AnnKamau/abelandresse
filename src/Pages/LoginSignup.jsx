8ad0e12dd586e21267e7a27aa8a97a99fd854195
AnnKamau <3annfaithk@gmail.com>
2024-10-23 16:42:30 +0300

    initializing login and signup page

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='User Name'/>
+          <input type="text" placeholder='Email Address'/>
+          <input type="password" placeholder='Password'/>
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an account? <span>Login Here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>By continuing, I agree to follow the terms of use & privacy policy</p>
+        </div>
+      </div>
     </div>
   )
 }
