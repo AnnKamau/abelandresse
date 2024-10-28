552f6a3103e7540106f2fd15c080bee5596bd305
AnnKamau <3annfaithk@gmail.com>
2024-10-28 14:24:04 +0300

    creating new banners

--- src/CarouselPage.js
+++ src/CarouselPage.js
@@ -1,35 +1,35 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-import banner2 from './Components/Assets/banner2.jpg'
-import banner3 from './Components/Assets/banner3.webp'
-import banner5 from './Components/Assets/banner5.jpg'
+import banner1 from './Components/Assets/1.png'
+import banner2 from './Components/Assets/2.png'
+import banner3 from './Components/Assets/3.png'
 
 const CarouselPage = () => {
   return (
     <div>
       <Carousel data-bs-theme="dark">
       <Carousel.Item>
-        <img
+        <img style={{height:'50vh'}}
           className="d-block w-100"
-          src={banner2}
+          src={banner1}
           alt="First slide"
         />
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
+      <img style={{height:'50vh'}}
           className="d-block w-100"
-          src={banner3}
+          src={banner2}
           alt="Second slide"
         />
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
+      <img style={{height:'50vh'}}
           className="d-block w-100"
-          src={banner5}
+          src={banner3}
           alt="Third slide"
         />
         <Carousel.Caption>
